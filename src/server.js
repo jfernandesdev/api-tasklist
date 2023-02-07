7e9353ddb6ba7699ac7abb02f69d80a9719cbdbb
jfernandesdev <jfernandes.dev@gmail.com>
2023-02-07 14:14:29 -0300

    FEAT: creating json middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,33 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World!')
+const tasks = []
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if(method === 'GET' && url === '/tasks') {
+    return res.end(JSON.stringify(tasks))
+  }
+
+  if(method === 'POST' && url === '/tasks') {
+    const { title, description } = req.body;
+
+    tasks.push({
+      id: 1,
+      title,
+      description,
+      completed_at: null,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
